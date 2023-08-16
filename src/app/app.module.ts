31e91eef1751b581cbc3b68649fbb7292784de5f
phtinh <phanhuutinh2002tg@gmail.com>
2023-08-16 15:32:47 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { OnSalePipe } from './on-sale.pipe';
 
 @NgModule({
-  declarations: [ AppComponent, HomeComponent ],
+  declarations: [ AppComponent, HomeComponent, OnSalePipe, OnSalePipe ],
   imports: [
     BrowserModule,
     AppRoutingModule,
